074d0a9dc79a058a4cfd4f16ab59c49bf9281f68
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-08-09 10:36:33 +0700

    config react native

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
 import {Text, View} from 'react-native';
 import {TailwindProvider} from 'tailwindcss-react-native';
+import { NativeRouter, Route, Routes, Link } from "react-router-native";
+import React from 'react';
+import Toast from 'react-native-toast-message';
+import OnBoard from './src/components/OnBoard';
 
 export default function App() {
     return (
-        <TailwindProvider>
-            <View className="flex-1 items-center justify-center bg-white">
-                <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-                <StatusBar style="auto"/>
-            </View>
-        </TailwindProvider>
+        <>
+            <NativeRouter>
+                <TailwindProvider>
+                        <View>
+                            <Routes>
+                                <Route path="/" element={<OnBoard />} />
+                            </Routes>
+                        </View>
+                </TailwindProvider>
+                <Toast />
+            </NativeRouter>
+        </>
     );
 }
---
+++ src/components/OnBoard.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, SafeAreaView, TextInput, Button, TouchableOpacity} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+const OnBoard = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-red-900">OnBorad</Text>
+        </SafeAreaView>
+    )
+}
+
+export default OnBoard;
